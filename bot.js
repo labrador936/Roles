13a3c88a55d0bf99d82fd3e300f0989aaaac668f
Atlantic Press <40704323+labrador936@users.noreply.github.com>
2019-11-26 15:23:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '$'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`by Floki |$roles `,"http://twitch.tv/S-F")
+client.user.setGame(`by Willson |$roles `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
